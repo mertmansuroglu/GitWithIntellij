6a4c065abd39734a3ca28bc0b6e1e016a85cc92a
mertmansuroglu <63956446+mertmansuroglu@users.noreply.github.com>
2022-02-01 02:18:52 +0300

    Update Deneme.java

--- src/main/java/deneme/Deneme.java
+++ src/main/java/deneme/Deneme.java
@@ -2,8 +2,8 @@ package deneme;
 
 public class Deneme {
     public static void main(String[] args) {
-        System.out.println("lokala gidecek olan merge");
-         System.out.println("remote gidecek olan merge");
+        System.out.println("pulla gidecek olan merge");
+         System.out.println("pulla gidecek olan merge");
         
 //    helloooooo
     }
